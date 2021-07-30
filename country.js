5dc9495ee0bb79f97a4078c69f1fae72e683b88c
TxnUkeGrl <txnukegrl@gmail.com>
2021-07-29 22:04:22 -0400

    the COLORS git add .

--- country.js
+++ country.js
@@ -6,24 +6,41 @@ class Country {
         this.colors = colors;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
 
+let usa = new Country("USA! USA!", "'Murican", "Hello World!", ["red", "white", "blue"]);
+let ukraine = new Country("Ukraine", "Ukrainian", "Privite, Narod!", ["blue", "gold", "black"]);
+let canada = new Country("Canada", "French Canadian", "Bonjour le monde!", ["red", "white", "black"])
 
-function SwitchCountry() {
-    let input = "USA";
+let countries = [usa, ukraine, canada];
+
+function SelectCountry(){
+    let input = "USA! USA!";
+    input = document.getElementById("CountryList").value;
     let country;
 
-    if (input === "USA") {
-        //set country to usa 
-        country = usa;
-    }
-    else if (input === "Mexico") {
-        country = mexico;
-    }
-    else if (input === "Algeria") {
-        country = algeria;
-    }
+if (input === "USA! USA!") {
+    //set country to usa 
+    country = usa;
+}
+else if (input === "Ukraine") {
+    country = ukraine;
+}
+else if (input === "Canada") {
+    country = canada;
+}
+DisplayColors(country);
+};
 
+function DisplayColors(country) {
+    let color1 = document.getElementById("Color1");
+    let color2 = document.getElementById("Color2");
+    let color3 = document.getElementById("Color3");
+    
+    document.getElementById("CountryName").innerText = country.name;
+    document.getElementById("OfficialLanguage").innerText = country.lang;
+    document.getElementById("HelloWorld").innerText = country.greeting;
+    
+    color1.style.backgroundColor = country.colors[0];
+    color2.style.backgroundColor = country.colors[1];
+    color3.style.backgroundColor = country.colors[2];
 }
